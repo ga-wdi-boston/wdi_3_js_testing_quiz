7f785015c72524df71d407fbe57146dd1ea96480
alik864 <alikarukas@gmail.com>
2015-05-11 14:23:42 -0400

    quiz

--- spec/quiz.spec.js
+++ spec/quiz.spec.js
@@ -1,3 +1,11 @@
+// Question 2.
+//
+// In the quiz.spec.js file, there is a test case with an
+// assertion that does not match the comments. Rewrite the
+// assertion so that it does what the comments say.
+
+
+
 describe('Question 2', function(){
 
   it('should strip zeroes from arrays', function(){
@@ -6,8 +14,10 @@ describe('Question 2', function(){
     var arrayWithoutZero = inputArray.filter(function(e){ return e !== 0; });
 
     // at this point the array should not have any element containing 0
-    expect().toEqual();
+    expect(inputArray.question2()).toEqual(3,2,1,4);
 
   });
 
 });
+
+
